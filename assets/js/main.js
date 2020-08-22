ec5ce2594ca4c45aa68575ebad9760277a0699c9
Samil <53584688+agostinhosamil@users.noreply.github.com>
2020-08-22 02:31:24 -0700

    Add Javascript files
    Add whole the .js files into the repository.

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+window.requestAnimationFrame = (
+	window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	(function (f) {
+		if ('function' === typeof f) f.apply(this, []);
+		setInterval (f, 500);
+	})
+);
+
+(function ( main ) {
+	var pageScrollEvent = function () {
+
+		var headerContainerHeight = (jQuery("#header").height()),
+			headerMenu = jQuery ('.header-menu').eq(0);
+
+		if (window.scrollY >= (headerContainerHeight + 10)) {
+			!headerMenu.hasClass('fixed-header-menu') && (
+				headerMenu.addClass('fixed-header-menu')
+			);
+		} else {
+			headerMenu.hasClass('fixed-header-menu') && (
+				headerMenu.removeClass('fixed-header-menu')
+			);
+		}
+
+		window.requestAnimationFrame (
+			pageScrollEvent
+		);
+	};
+
+	function init_js () {
+		pageScrollEvent ();
+
+		window.WOW && (new window.WOW().init({ offset: 100 }));
+
+		uolkeo.$$scut('shift+t', function () {
+			document.body.style.border = "100px solid #daa";
+		});
+
+
+
+		var swiper = new Swiper(".header-slider-self", {
+
+		    grabCursor: false,
+		    effect: "coverflow",
+		    slidesPerView: "auto",
+
+		    loop: true,
+
+		    "keyboard": {
+		    	"enabled": true
+		    }
+
+		});
+	}
+
+	window.manySliders = window.manySliders || {};
+	window.manySliders.sliders = [
+		'.x-product-demos-slider'
+	];
+
+	main.apply (window, [ init_js ]);	
+}( function ( js ) {
+	/**
+	 * [description]
+	 */
+	window.uolkeo && uolkeo.$$ready (function () {
+		js && js();
+	});
+}))
